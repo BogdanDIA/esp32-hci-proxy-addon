0eb1f428138bc09e001ea8ae61850e9f5afe173c
BogdanDIA <b_diaconescu@yahoo.com>
2026-03-15 18:49:13 +0200

    Update run-observer.sh

--- esp32-hci-proxy/run-observer.sh
+++ esp32-hci-proxy/run-observer.sh
@@ -1,6 +1,7 @@
 #!/bin/ash
 
 DOWN_COUNT=0
+res=""
 
 while :; do
   HCINUM=$(hciconfig | grep hci | wc -l)
@@ -11,17 +12,17 @@ while :; do
   if [[ $HCINUM -ge 0 ]]; then
     res=$(hciconfig hci${HCINUM} | grep DOWN)
     echo res: $res
-    if [[ -z $res ]]; then                   
-      echo "trying up: hci$HCINUM"      
+    if [[ ! -z $res ]]; then                   
+      echo "Trying hciconfig up: hci$HCINUM"      
       hciconfig hci${HCINUM} up        
       DOWN_COUNT=$(($DOWN_COUNT+1))    
       echo "DOWN_COUNT: $DOWN_COUNT"
       if [[ $DOWN_COUNT -ge 6 ]]; then   
         echo "killing btattach"    
-        killall -SIGKILL btattach           
-      else
-        DOWN_COUNT=0
-      fi                   
+        killall -SIGKILL btattach
+      fi
+    else
+      DOWN_COUNT=0
     fi
   fi
   sleep 20
