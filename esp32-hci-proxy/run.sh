063a8b5a906c71ea9ff1ccdfebba9ffec7067ebd
BogdanDIA <b_diaconescu@yahoo.com>
2026-03-15 15:20:02 +0200

    Update run.sh
    restart btattach in case the HCI cannot stay up.

--- esp32-hci-proxy/run.sh
+++ esp32-hci-proxy/run.sh
@@ -22,6 +22,28 @@ fi
 while :; do
   log_debug "serial ports: $(ls -al /dev/serial/by-path/)"
   log_debug "\n$(hciconfig)"
+  #obtain default HCINUM    
+  HCINUM=$(hciconfig | grep hci | wc -l)
+  HCINUM=$(($HCINUM-1))                 
+  echo "HCINUM: $HCINUM"                
+                        
+  #kill btattach after a number or tries
+  DOWN_COUNT=0                          
+                                            
+  if [[ $HCINUM -ge 0 ]]; then
+    res=$(hciconfig hci${HCINUM} | grep DOWN)
+    if [[ -n $res ]]; then                   
+      log_debug "trying up: hci$HCINUM"      
+      hciconfig hci${HCINUM} up        
+      DOWN_COUNT=$(($DOWN_COUNT+1))    
+      log_debug "DOWN_COUNT: $DOWN_COUNT"
+      if [[ $DOWN_COUNT -ge 6 ]]; then   
+        log_debug: "killing btattach"    
+        killall -9 btattach           
+      fi                             
+    fi  
+  fi  
+
   sleep 60
 done
 
